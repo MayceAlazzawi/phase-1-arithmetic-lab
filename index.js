b420a9b0d2ec0fc5752e8d4da9ba50be949b06b2
MayceAlazzawi <maynab4@gmail.com>
2022-04-24 15:39:42 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+
+const random = Math.floor(Math.random() * 10 +1);
+console.log(random)
+console.log(Number.isInteger(random))
+
+
+
+const num3= 64;
+const num4= 10;
+
+const mod = num3 % num4
+
+function max() {
+  const max = Math.max(1, 2, 3, 4, 20);
+return max
+}
